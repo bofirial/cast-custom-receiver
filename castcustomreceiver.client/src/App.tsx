c19f88d337fd64ce138fad719f3a31fe17645d03
James Schafer <jschafer@igsenergy.com>
2025-08-16 22:36:49 -0400

    Test

--- castcustomreceiver.client/src/App.tsx
+++ castcustomreceiver.client/src/App.tsx
@@ -5,61 +5,25 @@ function App() {
     const context = cast.framework.CastReceiverContext.getInstance();
     const playerManager = context.getPlayerManager();
 
-    function makeRequest(method: string, url: string) {
-        return new Promise(function (resolve, reject) {
-            const xhr = new XMLHttpRequest();
-            xhr.open(method, url);
-            xhr.onload = function () {
-                if (this.status >= 200 && this.status < 300) {
-                    resolve(JSON.parse(xhr.response));
-                } else {
-                    reject({
-                        status: this.status,
-                        statusText: xhr.statusText
-                    });
-                }
-            };
-            xhr.onerror = function () {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            };
-            xhr.send();
-        });
-    }
+    console.log(playerManager.broadcastStatus);
 
-    playerManager.setMessageInterceptor(
-        cast.framework.messages.MessageType.LOAD,
-        request => {
-            return new Promise((resolve, reject) => {
-                // Fetch content repository by requested contentId
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                makeRequest('GET', 'https://storage.googleapis.com/cpe-sample-media/content.json').then(function (data: any) {
-                    const item = data[request.media.contentId];
-                    if (!item) {
-                        // Content could not be found in repository
-                        reject();
-                    } else {
-                        // Adjusting request to make requested content playable
-                        request.media.contentUrl = item.stream.dash;
-                        request.media.contentType = 'application/dash+xml';
-                        // Add metadata
-                        const metadata = new
-                            cast.framework.messages.GenericMediaMetadata();
-                        metadata.title = item.title;
-                        metadata.subtitle = item.author;
+    const options = new cast.framework.CastReceiverOptions();
 
-                        request.media.metadata = metadata;
+    options.disableIdleTimeout = false;
+    options.customNamespaces = { 'urn:x-cast:rogueshambo': cast.framework.system.MessageType.STRING };
+    options.versionCode = 2;
 
-                        // Resolve request
-                        resolve(request);
-                    }
-                });
-            });
-        });
+    context.addCustomMessageListener('urn:x-cast:rogueshambo', function (event) {
+        console.log(event);
+        playerManager.pause();
+    });
 
-    context.start();
+    //{
+    //    customNamespaces: { 'urn:x-cast:rogueshambo': cast.framework.system.MessageType.STRING },
+    //    disableIdleTimeout: false,
+    //        versionCode: 1
+    //}
+    context.start(options);
 
     return (
         <div>
